2156f2d6b2d275141a15ef1d5678d6a49f070594
+ <+>
2023-04-04 17:34:33 +0300

    Completed assignment

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const filmsUrl = " http://localhost:3000/films";
+
+
+
+//Fetch fist movie details
+function fetchFirstMovie (id) {
+    fetch(`${filmsUrl}/${id}`)
+    .then(response => response.json())
+    .then(movie => {
+        document.getElementById("poster").src = movie.poster;
+        document.getElementById("title").innerHTML = movie.title;
+        document.getElementById("runtime").innerHTML = movie.runtime;
+        document.getElementById("film-info").innerHTML = movie.description;
+        document.getElementById("showtime").innerHTML = movie.showtime;
+        document.getElementById("ticket-num").innerHTML = movie.capacity - movie.tickets_sold;
+    })
+}
+//Fetch movie titles
+function fetchMovieTitles () {
+    return fetch(`${filmsUrl}`)
+    .then(response => response.json())
+    //.then(data => console.log(data));
+}
+//Display movie titles
+function renderMovieTitles (movieTitles) {
+    const movieList = document.getElementById("films");
+    const movies = document.createElement("li");
+    movies.innerHTML = movieTitles.title.toUpperCase();
+    movieList.appendChild(movies);
+}
